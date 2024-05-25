b545e5c3540a7bac676dbe2996211cfb3d8001b5
larizl <161778428+larizl@users.noreply.github.com>
2024-05-24 21:11:34 -0400

    Add files via upload
    negocio do nome dos alunos

---
+++ javascript/recursos/js/app.js
@@ -0,0 +1,11 @@
+console.log(cabecalho)
+
+Function pegarDadosDoAluno(){
+  const nome document.getElementById("nome")
+  const not1 document.getElementById("nome")
+  const not2 document.getElementById("nome")
+  const not3 document.getElementById("nome")
+console.log(nome)
+}
+
+pegarDadosDoAluno()
